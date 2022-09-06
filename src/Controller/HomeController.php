6a4cecb3986f58ee6c4c30e6ff0d8f6c32e51525
Alexandr Malozemov <is.malozemov@ya.ru>
2022-09-06 16:06:35 +0500

    Completed converter and action method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use League\CommonMark\CommonMarkConverter;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private readonly CommonMarkConverter $converter) {}
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/markdown', name: 'app_markdown_convert', methods: 'POST')]
+    public function markdown(Request $request): Response
+    {
+        $markdown = $request->get('markdown') ?? '';
+        $html = $this->converter->convert($markdown);
+
+        return new Response($html->getContent());
     }
 }
